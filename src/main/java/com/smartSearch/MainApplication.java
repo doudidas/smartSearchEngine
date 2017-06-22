0556f3747201b7ca7f2e291ba1125fa74ca262b4
Edouard <edouard.topin@telecom-sudparis.eu>
2017-06-22 23:50:27 +0200

    security removed…

--- src/main/java/com/smartSearch/MainApplication.java
+++ src/main/java/com/smartSearch/MainApplication.java
@@ -6,10 +6,14 @@ import io.dropwizard.Application;
 import io.dropwizard.assets.AssetsBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 import resources.DestinationResource;
 import resources.UserResource;
 
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
 import java.net.UnknownHostException;
+import java.util.EnumSet;
 
 public class MainApplication extends Application<MainConfiguration> {
 
@@ -32,6 +36,18 @@ public class MainApplication extends Application<MainConfiguration> {
 
     @Override
     public void run(MainConfiguration mainConfiguration, Environment environment) throws UnknownHostException {
+        // Enable CORS headers
+        final FilterRegistration.Dynamic cors =
+                environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+
+        // Configure CORS parameters
+        cors.setInitParameter("allowedOrigins", "*");
+        cors.setInitParameter("allowedHeaders", "X-Requested-With,Content-Type,Accept,Origin");
+        cors.setInitParameter("allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+
+        // Add URL mapping
+        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+
         MongoClient mongoClient = new MongoClient("localhost", 27017);
         MongoDatabase database = mongoClient.getDatabase("SmartSearchDatabase");
         environment.healthChecks().register("mongo", new MongoHealthCheck(mongoClient));
