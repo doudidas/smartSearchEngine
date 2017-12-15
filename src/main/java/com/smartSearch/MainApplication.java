a8f174fc28eb3e3d74722842cf34e5040326caba
Edouard Topin <ed.topin@gmail.com>
2017-12-15 18:06:51 +0100

    Update MainApplication.java

--- src/main/java/com/smartSearch/MainApplication.java
+++ src/main/java/com/smartSearch/MainApplication.java
@@ -49,7 +49,7 @@ public class MainApplication extends Application<MainConfiguration> {
         // Add URL mapping
         cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
 
-        MongoClient mongoClient = new MongoClient("server-mongo", 27017);
+        MongoClient mongoClient = new MongoClient("mongo-server.lab.local", 27017);
         MongoDatabase database = mongoClient.getDatabase("SmartSearchDatabase");
         environment.healthChecks().register("mongo", new MongoHealthCheck(mongoClient));
         final UserResource userResource = new UserResource(database);
