e4c93c4ef4d71a7f312a2b44297e339e5da61fdf
Edouard Topin <ed.topin@gmail.com>
2018-05-02 14:49:33 +0200

    merge fix

--- src/main/java/resources/DefaultResource.java
+++ src/main/java/resources/DefaultResource.java
@@ -6,10 +6,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-<<<<<<< HEAD
-=======
-
->>>>>>> 5f7f252250c40cb6dfdd2ee4f3d16ecdce339be2
 
 @Path("/")
 @Produces(MediaType.APPLICATION_JSON)
@@ -17,17 +13,10 @@ import javax.ws.rs.core.Response;
 public class DefaultResource {
   public DefaultResource() {
   }
-<<<<<<< HEAD
   @GET
   public Response sayHi() {
       JSONObject responseBody = new JSONObject();
       responseBody.put("message", "hello");
       return Response.status(Response.Status.ACCEPTED).entity(responseBody).build();
   }
-=======
-    @GET
-    public Response sayHi() {
-        return Response.status(Response.Status.ACCEPTED).build();
-    }
->>>>>>> 5f7f252250c40cb6dfdd2ee4f3d16ecdce339be2
 }
