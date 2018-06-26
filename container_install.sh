9dfff03b6ff37e9475b90cf0ead54d5b769dac49
Edouard Topin <ed.topin@gmail.com>
2018-06-26 17:49:57 +0200

    fixed ?

--- container_install.sh
+++ container_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-mvn package
-mv ./target/API.jar ./API.jar
-mv src/main/resources/smartSearch.yaml .
-mvn clean
+mvn package;
+mv ./target/API.jar ./API.jar;
+mv src/main/resources/smartSearch.yaml; .
+mvn clean;
 rm -v !("API.jar"|"smartSearch.yaml");
